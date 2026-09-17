3a93b1fb4ab31b51ec13fa612a64ff69dd9705f6
marcustwest77-collab <marcustwest77@gmail.com>
2026-09-17 03:35:06 -0500

    Add factory preset bank API

--- Source/PresetManager.h
+++ Source/PresetManager.h
@@ -1,4 +1,20 @@
 #pragma once
 #include <JuceHeader.h>
-class PresetManager{public:explicit PresetManager(juce::AudioProcessorValueTreeState&s):state(s){}bool save(const juce::String&);bool load(const juce::String&);juce::StringArray list()const;
-private:juce::AudioProcessorValueTreeState&state;juce::File dir()const;};
+
+class PresetManager
+{
+public:
+    explicit PresetManager(juce::AudioProcessorValueTreeState& s) : state(s) {}
+
+    bool save(const juce::String&);
+    bool load(const juce::String&);
+    juce::StringArray list() const;
+
+    juce::StringArray factoryPresetNames() const;
+    bool applyFactoryPreset(const juce::String& name);
+
+private:
+    juce::AudioProcessorValueTreeState& state;
+    juce::File dir() const;
+    void setParameter(const juce::String& id, float value);
+};
